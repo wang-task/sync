1c656cceddd4b6415f326204b2d37b94e1334b99
sunnywangzi <88769439+sunnywangzi@users.noreply.github.com>
2022-12-02 22:48:42 +0800

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rclone sync onedrive-wceshi: onedrive-backup:backup --transfers 48 -v --ignore-errors
+rclone sync onedrive-wceshi: onedrive-backup:backup --transfers 48 -v --ignore-errors --checkers 64
